0711b6ec96389e576ab46797d154e02c94d97fab
strai419 <67533334+strai419@users.noreply.github.com>
2020-07-14 18:39:16 +0900

    Add files via upload

---
+++ JavaScript_Primer/index.js
@@ -0,0 +1,44 @@
+// 1;
+// console.log(1+1);
+
+const total = 42 + 42;
+console.log(total);
+
+console.log(1); // => SyntaxError: missing ) after argument list
+
+const a = 1;
+
+const value = "値";
+console.log(value);
+
+console.log(typeof true);
+console.log(typeof 42);
+console.log(typeof "JavaScript");
+console.log(typeof Symbol("シンボル"));
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof ["配列"]);
+console.log(typeof {"Key": "value"});
+console.log(typeof function(){});
+
+// "と"で囲んだ範囲が文字列リテラル
+const str = "こんにちは";
+console.log(str);
+
+true;
+false;
+
+console.log(0b1111);
+console.log(0b10000000000);
+console.log(0o644);
+console.log(0o777);
+
+//　非推奨な８進数の書き方
+//　strict modeは例外が発生
+console.log(0644);
+console.log(0777);
+
+console.log(0xFF);
+console.log(0xff);
+console.log(0x30A2);
+
